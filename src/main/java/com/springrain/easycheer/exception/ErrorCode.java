20ffbf33235606b7909fa83fc3351be210b553c4
Jia Chunyu <jiachunyu@gmail.com>
2017-08-14 07:10:52 +0800

    Continue working on error handling.

---
+++ src/main/java/com/springrain/easycheer/exception/ErrorCode.java
@@ -0,0 +1,7 @@
+package com.springrain.easycheer.exception;
+
+public interface ErrorCode {
+	
+	public static final int TENANT_NOT_FOUND = 1;
+
+}
---
+++ src/main/java/com/springrain/easycheer/rest/RestExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.springrain.easycheer.rest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler
+	@ResponseBody
+	ResponseEntity<?> handleException(HttpServletRequest request, Throwable e) {
+		return null;
+	}
+}
