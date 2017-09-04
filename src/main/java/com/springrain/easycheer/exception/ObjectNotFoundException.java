3bf997772d3f959a9a59e83abd5f8f9857e09836
Jia Chunyu <jiachunyu@gmail.com>
2017-09-04 22:55:08 +0800

    Minor change on exception handling.

--- src/main/java/com/springrain/easycheer/exception/ObjectNotFoundException.java
+++ src/main/java/com/springrain/easycheer/exception/ObjectNotFoundException.java
@@ -1,6 +1,6 @@
 package com.springrain.easycheer.exception;
 
-public class ObjectNotFoundException extends RestBaseException {
+public class ObjectNotFoundException extends BaseException {
 
 	public ObjectNotFoundException(int errorCode, String errorMessage) {
 		super(errorCode, errorMessage);
